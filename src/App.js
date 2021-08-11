1d51cf1e467a9ecd3a259a027947f3a6c37dafd9
Seong-uk-An <asu970602@naver.com>
2021-08-11 17:00:35 +0900

    fucking tired

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ class App extends React.Component {
   state = {
     count: 0,
   };
+
   add = () => {
-    this.setState({ count: 1 });
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
   };
+
   minus = () => {
-    this.setState({ count: -1 });
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
   };
+
   render() {
     return (
       <div>
